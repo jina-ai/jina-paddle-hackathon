a515f7389ecb7e772202974fa203ce96a73b48c0
fan xia <gonorth@qq.com>
2023-03-11 15:30:30 +0800

    plato

--- src/uie/src/flow.py
+++
@@ -1,9 +0,0 @@
-from jina import Flow
-from uie_executor.executor import UIEExecutor
-
-
-f = Flow(port=[12345, 12344, 12346], protocol=["http", "grpc", "websocket"], cors=True).add(
-    name='uie', uses=UIEExecutor)
-
-with f:
-    f.block()
